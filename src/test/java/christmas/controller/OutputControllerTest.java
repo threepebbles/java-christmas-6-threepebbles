1ae4086fb90fc8dad44bb893fcf6529c14267c6c
heiler <threepebbles@naver.com>
2023-11-11 03:53:26 +0900

    Test(OutputCOntrollerTest): 테스트 통과 조건을 strict하게 수정

--- src/test/java/christmas/controller/OutputControllerTest.java
+++ src/test/java/christmas/controller/OutputControllerTest.java
@@ -21,6 +21,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class OutputControllerTest {
+    public static final String LINE_SEPARATOR = System.lineSeparator();
     private OutputController outputController;
     private ByteArrayOutputStream output;
 
@@ -52,7 +53,7 @@ public class OutputControllerTest {
         String actual = output.toString();
 
         // then
-        assertThat(actual).contains(String.format("12월 %d일", day));
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("주문 메뉴가 정상적으로 출력되는지 테스트한다.")
@@ -62,27 +63,34 @@ public class OutputControllerTest {
         Order order = new Order();
         order.addMenu("티본스테이크", 2);
         order.addMenu("제로콜라", 2);
+        String expected = LINE_SEPARATOR + LINE_SEPARATOR
+                + "<주문 메뉴>" + LINE_SEPARATOR
+                + "제로콜라 2개" + LINE_SEPARATOR
+                + "티본스테이크 2개" + LINE_SEPARATOR;
 
         // when
         outputController.printOrder(order);
         String actual = output.toString();
 
         // then
-        assertThat(actual).contains("<주문 메뉴>", "제로콜라 2개", "티본스테이크 2개");
+        assertThat(actual).isEqualTo(expected);
     }
 
-    @DisplayName("할인 전 총 주문 금액이 정상적으로 출력되는지 테스트한다.")
+    @DisplayName("할인 전 총주문 금액이 정상적으로 출력되는지 테스트한다.")
     @Test
     void 할인_전_총_주문_금액_출력_테스트() {
         // given
         int amount = 100203;
+        String expected = LINE_SEPARATOR
+                + "<할인 전 총주문 금액>" + LINE_SEPARATOR
+                + "100,203원" + LINE_SEPARATOR;
 
         // when
         outputController.printTotalPriceBeforeDiscount(amount);
         String actual = output.toString();
 
         // then
-        assertThat(actual).contains("<할인 전 총주문 금액>", "100,203원");
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("증정 메뉴가 있는 경우 정상적으로 출력되는지 테스트한다.")
@@ -90,13 +98,16 @@ public class OutputControllerTest {
     void 증정_메뉴_있는_경우_출력_테스트() {
         // given
         Gift gift = Gift.CHAMPAGNE;
+        String expected = LINE_SEPARATOR
+                + "<증정 메뉴>" + LINE_SEPARATOR
+                + "샴페인 1개" + LINE_SEPARATOR;
 
         // when
         outputController.printGift(gift);
         String actual = output.toString();
 
         // then
-        assertThat(actual).contains("<증정 메뉴>", "샴페인 1개");
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("증정 메뉴가 없는 경우 정상적으로 출력되는지 테스트한다.")
@@ -104,13 +115,16 @@ public class OutputControllerTest {
     void 증정_메뉴_없는_경우_출력_테스트() {
         // given
         Gift gift = Gift.NOTHING;
+        String expected = LINE_SEPARATOR
+                + "<증정 메뉴>" + LINE_SEPARATOR
+                + "없음" + LINE_SEPARATOR;
 
         // when
         outputController.printGift(gift);
         String actual = output.toString();
 
         // then
-        assertThat(actual).contains("<증정 메뉴>", "없음");
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("혜택 내역이 있는 경우 정상적으로 출력되는지 테스트한다.")
@@ -122,13 +136,18 @@ public class OutputControllerTest {
             add(new Discount(DiscountType.SPECIAL, 2020));
             add(new Discount(DiscountType.GIFT, 3030));
         }};
+        String expected = LINE_SEPARATOR
+                + "<혜택 내역>" + LINE_SEPARATOR
+                + "크리스마스 디데이 할인: -1,010원" + LINE_SEPARATOR
+                + "특별 할인: -2,020원" + LINE_SEPARATOR
+                + "증정 이벤트: -3,030원" + LINE_SEPARATOR;
 
         // when
         outputController.printDiscountDetails(discounts);
         String actual = output.toString();
 
         // then
-        assertThat(actual).contains("<혜택 내역>", "크리스마스 디데이 할인: -1,010원", "특별 할인: -2,020원", "증정 이벤트: -3,030원");
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("총혜택 금액이 정상적으로 출력되는지 테스트한다.")
@@ -136,13 +155,16 @@ public class OutputControllerTest {
     void 총혜택_금액_출력_테스트() {
         // given
         int totalDiscount = 102030;
+        String expected = LINE_SEPARATOR
+                + "<총혜택 금액>" + LINE_SEPARATOR
+                + "-102,030원" + LINE_SEPARATOR;
 
         // when
         outputController.printTotalDiscount(totalDiscount);
         String actual = output.toString();
 
-        // then
-        assertThat(actual).contains("<총혜택 금액>", "102,030원");
+        // then;
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("할인 후 예상 결제 금액이 정상적으로 출력되는지 테스트한다.")
@@ -150,13 +172,16 @@ public class OutputControllerTest {
     void 할인_후_예상_결제_금액_출력_테스트() {
         // given
         int expectedPay = 52000;
+        String expected = LINE_SEPARATOR
+                + "<할인 후 예상 결제 금액>" + LINE_SEPARATOR
+                + "52,000원" + LINE_SEPARATOR;
 
         // when
         outputController.printExpectedPayAfterDiscount(expectedPay);
         String actual = output.toString();
 
         // then
-        assertThat(actual).contains("<할인 후 예상 결제 금액>", "52,000원");
+        assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("12월 이벤트 배지가 정상적으로 출력되는지 테스트한다.")
@@ -164,12 +189,15 @@ public class OutputControllerTest {
     void 이벤트_배지_출력_테스트() {
         // given
         EventBadge eventBadge = EventBadge.STAR;
+        String expected = LINE_SEPARATOR
+                + "<12월 이벤트 배지>" + LINE_SEPARATOR
+                + "별" + LINE_SEPARATOR;
 
         // when
         outputController.printEventBadge(eventBadge);
         String actual = output.toString();
 
         // then
-        assertThat(actual).contains("<12월 이벤트 배지>", "별");
+        assertThat(actual).isEqualTo(expected);
     }
 }
