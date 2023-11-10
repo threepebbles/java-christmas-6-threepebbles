93213d23e52c63e72daa65f38c1496b42886010c
heiler <threepebbles@naver.com>
2023-11-11 00:17:50 +0900

    Refactor(OuputView): 클래스 내부 리팩토링

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -12,53 +12,51 @@ public class OutputView {
     public static final String EVENT_BADGE_FORMAT = "<12월 이벤트 배지>%s";
 
     public void printEventStatisticsHeader(String dateText) {
-        String text = String.format(EVENT_STATISTICS_HEADER_FORMAT, dateText);
-        System.out.print(text);
+        printByFormat(EVENT_STATISTICS_HEADER_FORMAT, dateText);
     }
 
     public void printOrder(String orderText) {
-        String text = LINE_SEPARATOR.repeat(2)
-                + String.format(ORDER_FORMAT, orderText);
-        System.out.print(text);
+        printLineSpacing(2);
+        printByFormat(ORDER_FORMAT, orderText);
     }
 
     public void printTotalPriceBeforeDiscount(String amountText) {
-        String text = LINE_SEPARATOR.repeat(2)
-                + String.format(TOTAL_PRICE_BEFORE_DISCOUNT_FORMAT, amountText);
-        System.out.print(text);
+        printLineSpacing(2);
+        printByFormat(TOTAL_PRICE_BEFORE_DISCOUNT_FORMAT, amountText);
     }
 
     public void printGift(String giftText) {
-        String text = LINE_SEPARATOR.repeat(2)
-                + String.format(GIFT_FORMAT, giftText);
-        System.out.print(text);
+        printLineSpacing(2);
+        printByFormat(GIFT_FORMAT, giftText);
     }
 
     public void printDiscountDetails(String detailsText) {
-        String text = LINE_SEPARATOR.repeat(2)
-                + String.format(DISCOUNT_DETAILS_FORMAT, detailsText);
-        System.out.print(text);
+        printLineSpacing(2);
+        printByFormat(DISCOUNT_DETAILS_FORMAT, detailsText);
     }
 
     public void printTotalDiscount(String totalDiscountText) {
-        String text = LINE_SEPARATOR.repeat(2)
-                + String.format(TOTAL_DISCOUNT_FORMAT, totalDiscountText);
-        System.out.print(text);
+        printLineSpacing(2);
+        printByFormat(TOTAL_DISCOUNT_FORMAT, totalDiscountText);
     }
 
     public void printExpectedPayAfterDiscount(String expectedPayText) {
-        String text = LINE_SEPARATOR.repeat(2)
-                + String.format(EXPECTED_PAY_AFTER_DISCOUNT_FORMAT, expectedPayText);
-        System.out.print(text);
+        printLineSpacing(2);
+        printByFormat(EXPECTED_PAY_AFTER_DISCOUNT_FORMAT, expectedPayText);
     }
 
     public void printEventBadge(String eventBadgeText) {
-        String text = LINE_SEPARATOR.repeat(2)
-                + String.format(EVENT_BADGE_FORMAT, eventBadgeText);
-        System.out.print(text);
+        printLineSpacing(2);
+        printByFormat(EVENT_BADGE_FORMAT, eventBadgeText);
     }
 
-    public void printText(String text) {
-        System.out.print(text);
+
+    public void printByFormat(String format, String... args) {
+        printLineSpacing(2);
+        System.out.printf(format, args);
+    }
+
+    public void printLineSpacing(int repeat) {
+        System.out.print(LINE_SEPARATOR.repeat(repeat));
     }
 }
