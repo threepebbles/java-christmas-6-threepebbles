3e47389d8d6cd5a5357974371f15cb2e46cba9de
heiler <threepebbles@naver.com>
2023-11-10 10:59:02 +0900

    Feat(Menu): 화폐 단위 추가

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -1,25 +1,27 @@
 package christmas.domain;
 
 public enum Menu {
-    MUSHROOM_SOUP("양송이수프", 6000),
-    TAPAS("타파스", 5500),
-    CAESAR_SALAD("시저샐러드", 8000),
-    T_BONE_STEAK("티본스테이크", 55000),
-    BARBECUE_LIP("바비큐립", 54000),
-    SEAFOOD_PASTA("해산물파스타", 35000),
-    CHRISTMAS_PASTA("크리스마스파스타", 25000),
-    CHOCOLATE_CAKE("초코케이크", 15000),
-    ICE_CREAM("아이스크림", 5000),
-    ZERO_COKE("제로콜라", 3000),
-    RED_WINE("레드와인", 60000),
-    CHAMPAGNE("샴페인", 25000);
+    MUSHROOM_SOUP("양송이수프", 6000, "원"),
+    TAPAS("타파스", 5500, "원"),
+    CAESAR_SALAD("시저샐러드", 8000, "원"),
+    T_BONE_STEAK("티본스테이크", 55000, "원"),
+    BARBECUE_LIP("바비큐립", 54000, "원"),
+    SEAFOOD_PASTA("해산물파스타", 35000, "원"),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000, "원"),
+    CHOCOLATE_CAKE("초코케이크", 15000, "원"),
+    ICE_CREAM("아이스크림", 5000, "원"),
+    ZERO_COKE("제로콜라", 3000, "원"),
+    RED_WINE("레드와인", 60000, "원"),
+    CHAMPAGNE("샴페인", 25000, "원");
 
     private String name;
     private int price;
+    private String currencyUnit;
 
-    Menu(String name, int price) {
+    Menu(String name, int price, String currencyUnit) {
         this.name = name;
         this.price = price;
+        this.currencyUnit = currencyUnit;
     }
 
     public static Menu findMenuByName(String name) {
@@ -42,5 +44,9 @@ public enum Menu {
 
     public int getPrice() {
         return price;
+    }
+
+    public String getCurrencyUnit() {
+        return currencyUnit;
     }
 }
