daabd0a37e1ea6e4ef45068cae68dce9faf72937
heiler <threepebbles@naver.com>
2023-11-14 22:46:04 +0900

    Test: GiftEvent 도메인 로직 테스트 코드 작성

---
+++ src/test/java/christmas/domain/GiftEventTest.java
@@ -0,0 +1,64 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.domain.constant.Gift;
+import christmas.domain.constant.Menu;
+import christmas.domain.event.GiftEvent;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class GiftEventTest {
+    @Test
+    void 증정_메뉴_증정하는_경우_테스트() {
+        // given
+        List<Order> orderList = new ArrayList<>() {{
+            add(new Order(Menu.BARBECUE_LIP, 2));   // 108000원
+            add(new Order(Menu.ZERO_COKE, 4));      // 12000원
+        }};
+        Orders orders = new Orders(orderList);
+        GiftEvent giftEvent = new GiftEvent(orders);
+
+        // when
+        Gift gift = giftEvent.requestGift();
+
+        // then
+        assertThat(gift).isEqualTo(Gift.CHAMPAGNE);
+    }
+
+    @Test
+    void 증정_메뉴_증정하지_않는_경우_테스트() {
+        // given
+        List<Order> orderList = new ArrayList<>() {{
+            add(new Order(Menu.BARBECUE_LIP, 2));   // 108000원
+            add(new Order(Menu.ZERO_COKE, 3));      // 9000원
+        }};
+        Orders orders = new Orders(orderList);
+        GiftEvent giftEvent = new GiftEvent(orders);
+
+        // when
+        Gift gift = giftEvent.requestGift();
+
+        // then
+        assertThat(gift).isEqualTo(Gift.NOTHING);
+    }
+
+    @Test
+    void 증정_메뉴_가격_계산_테스트() {
+        // given
+        List<Order> orderList = new ArrayList<>() {{
+            add(new Order(Menu.BARBECUE_LIP, 2));   // 108000원
+            add(new Order(Menu.ZERO_COKE, 4));      // 12000원
+        }};
+        Orders orders = new Orders(orderList);
+        GiftEvent giftEvent = new GiftEvent(orders);
+
+        // when
+        int actual = giftEvent.calculateDiscountResult()
+                .getAmount();
+
+        // then
+        assertThat(actual).isEqualTo(Gift.CHAMPAGNE.getPrice());
+    }
+}
---
+++ src/test/java/christmas/domain/WeekendDiscountEventTest.java
@@ -0,0 +1,10 @@
+package christmas.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class WeekendDiscountEventTest {
+    @Test
+    void 주말_할인가_계산_테스트() {
+        
+    }
+}
