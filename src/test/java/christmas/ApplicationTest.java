bd0bab4a66becfb47ba7b52622dfd248e5bdc89f
heiler <threepebbles@naver.com>
2023-11-15 07:10:29 +0900

    Test(ApplicationTest): 기존에 작성된 테스트 함수 원복

--- src/test/java/christmas/ApplicationTest.java
+++ src/test/java/christmas/ApplicationTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ApplicationTest extends NsTest {
     private static final String LINE_SEPARATOR = System.lineSeparator();
@@ -36,16 +38,35 @@ class ApplicationTest extends NsTest {
     @Test
     void 날짜_예외_테스트() {
         assertSimpleTest(() -> {
-            runException("", "0", "32", "first", "1일", "이십일");
+            runException("a");
             assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         });
     }
 
     @Test
     void 주문_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("3", "제로콜라-a");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    // 추가 테스트
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "\t", "0", "32", "first", "1일", "이십일"})
+    void 방문_날짜_입력_예외_테스트(String dayOfVisit) {
+        assertSimpleTest(() -> {
+            runException(dayOfVisit);
+            assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 주문_목록_입력_예외_테스트() {
         String day = "1";
         assertSimpleTest(() -> {
             runException(day, "",
+                    day, "제로콜라-",
                     day, "제로콜라:일",
                     day, "제로콜라-하나",
                     day, "제로콜라1",
@@ -56,7 +77,7 @@ class ApplicationTest extends NsTest {
 
     // 추가 테스트
     @Test
-    void 증정_메뉴_없음_출력() {
+    void 증정_메뉴_없음_출력_테스트() {
         assertSimpleTest(() -> {
             run("10", "타파스-1,제로콜라-1");
             assertThat(output()).contains("<증정 메뉴>" + LINE_SEPARATOR + "없음");
@@ -64,7 +85,7 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 이벤트_배지_없음_출력() {
+    void 이벤트_배지_없음_출력_테스트() {
         assertSimpleTest(() -> {
             run("31", "타파스-1,제로콜라-1");
             assertThat(output()).contains("<12월 이벤트 배지>" + LINE_SEPARATOR + "없음");
