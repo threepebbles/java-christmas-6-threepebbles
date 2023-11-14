596947dbb710d94ca872ebbcd4254d8ec47d60c9
heiler <threepebbles@naver.com>
2023-11-14 23:24:37 +0900

    Test: 이벤트 배지 부여하는 로직 테스트 코드 작성

---
+++ src/test/java/christmas/domain/EventBadgeTest.java
@@ -0,0 +1,46 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.domain.constant.EventBadge;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class EventBadgeTest {
+    @DisplayName("배지를 부여 하지 않는 경우")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1000, 2023, 4999})
+    void 배지를_부여하지_않는_경우_테스트(int totalDiscount) {
+        // when & then
+        assertThat(EventBadge.getEventBadgeByTotalDiscount(totalDiscount))
+                .isEqualTo(EventBadge.NOTHING);
+    }
+
+    @DisplayName("별 배지를 부여하는 경우")
+    @ParameterizedTest
+    @ValueSource(ints = {5000, 9999})
+    void 별_배지_부여하는_경우_테스트(int totalDiscount) {
+        // when & then
+        assertThat(EventBadge.getEventBadgeByTotalDiscount(totalDiscount))
+                .isEqualTo(EventBadge.STAR);
+    }
+
+    @DisplayName("트리 배지를 부여하는 경우")
+    @ParameterizedTest
+    @ValueSource(ints = {10000, 19999})
+    void 트리_배지_부여하는_경우_테스트(int totalDiscount) {
+        // when & then
+        assertThat(EventBadge.getEventBadgeByTotalDiscount(totalDiscount))
+                .isEqualTo(EventBadge.TREE);
+    }
+
+    @DisplayName("산타 배지를 부여하는 경우")
+    @ParameterizedTest
+    @ValueSource(ints = {20000, 30000})
+    void 산타_배지_부여하는_경우_테스트(int totalDiscount) {
+        // when & then
+        assertThat(EventBadge.getEventBadgeByTotalDiscount(totalDiscount))
+                .isEqualTo(EventBadge.SANTA);
+    }
+}
