d59b93bb0e9610a15dc1cd8ca14e7f99259c6a42
heiler <threepebbles@naver.com>
2023-11-14 23:09:05 +0900

    Test: Orders 도메인 로직 테스트 코드 작성

---
+++ src/test/java/christmas/domain/OrdersTest.java
@@ -0,0 +1,49 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.domain.constant.Menu;
+import christmas.domain.constant.MenuType;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class OrdersTest {
+    @Test
+    void 총가격_계산_테스트() {
+        // given
+        List<Order> orderList = new ArrayList<>() {{
+            add(new Order(Menu.BARBECUE_LIP, 2));   // 108000원
+            add(new Order(Menu.ZERO_COKE, 3));      // 9000원
+        }};
+        Orders orders = new Orders(orderList);
+
+        int expected = 117000;
+
+        // when
+        int actual = orders.calculateTotalPrice();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void 특정_메뉴_종류_개수_계산_테스트() {
+        // given
+        List<Order> orderList = new ArrayList<>() {{
+            add(new Order(Menu.BARBECUE_LIP, 2));   // 메인 2개
+            add(new Order(Menu.ZERO_COKE, 3));
+            add(new Order(Menu.CHRISTMAS_PASTA, 4));    // 메인 4개
+            add(new Order(Menu.CHOCOLATE_CAKE, 3));
+        }};
+        Orders orders = new Orders(orderList);
+
+        int expected = 6;
+
+        // when
+        int actual = orders.countByMenuType(MenuType.MAIN);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+}
